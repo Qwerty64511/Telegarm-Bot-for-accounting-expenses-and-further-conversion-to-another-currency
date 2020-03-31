862e5629dc1ef3c0f4313c3859d5b21a894b258b
Qwerty64511 <semnov_matvey@mail.ru>
2020-03-31 21:40:27 +0300

    REdis

--- bot0.py
+++ bot0.py
@@ -1,5 +1,27 @@
 import os
 import telebot
+import redis
+import json
+
+REDIS_URL = os.environ.get('REDIS_URL')
+dict_db = {}
+
+
+def save(key, value):
+    if REDIS_URL:
+        redis_db = redis.from_url(REDIS_URL)
+        redis_db.set(key, value)
+    else:
+        dict_db[key] = value
+
+
+def load(key, value):
+    if REDIS_URL:
+        redis_db = redis.from_url(REDIS_URL)
+        return redis_db.get(key)
+    else:
+        return dict_db.get(key)
+
 
 token = os.environ["TELEGRAM_TOKEN"]
 
@@ -7,13 +29,28 @@ bot = telebot.TeleBot(token)
 
 IS_HEROKU = os.environ.get('IS_HEROKU', False)
 
-ADMIN = (0, 1, 2)
+
+# MAIN_STATE = 'main'
+# DATE_STATE = 'date_state'
 
 
 @bot.message_handler(content_types=['text'])
 def start(message):
+    # save('state:{user_id}'.format(user_id=message.from_user.id), MAIN_STATE)
+    # save(str(message.from_user.id), MAIN_STATE)
+    # user_state = load('state:{user_id}'.format(user_id=message.from_user.id))
+    # user_state = str(message.from_user.id)
+    #
+    #   my_dict = {'a':10, 'b': 'xyz'}
+    #
+    #   save('key', my_dict)
+    #
+    #  my_dict_str = json.dumps(my_dict)
+    #   save('key', my_dict_str)
+    #  my_dict = json.loads(load('key'))
     if IS_HEROKU:
-        bot.send_message(message.from_user.id, 'Я на heroku, вероятно создатель теперь не полный ебанат в проганьи')
+        bot.send_message(message.from_user.id, 'Я на heroku, вероятно создатель теперь '
+                                               'не полный ебанат в проганьи\n' + str(REDIS_URL))
 
     if message.text == '/start':
         bot.send_message(message.from_user.id, 'Я подопытный кролик')
