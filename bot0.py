5aa3b729e92d03d00e69e59de371585b0e98013e
Qwerty64511 <semnov_matvey@mail.ru>
2020-03-31 20:33:53 +0300

    Переименовали переменную

--- bot0.py
+++ bot0.py
@@ -3,16 +3,16 @@ import telebot
 
 token = os.environ["TELEGRAM_TOKEN"]
 
-
 bot = telebot.TeleBot(token)
 
+IS_HEROKU = os.environ.get('IS_HEROKU', False)
 
-HEROKU = os.environ.get('HEROKU', False)
+ADMIN = (0, 1, 2)
 
 
 @bot.message_handler(content_types=['text'])
 def start(message):
-    if HEROKU:
+    if IS_HEROKU:
         bot.send_message(message.from_user.id, 'Я на heroku, вероятно создатель теперь не полный ебанат в проганьи')
 
     if message.text == '/start':
