166acf01641262e9c161a34f9d1d32338abafc25
Qwerty64511 <semnov_matvey@mail.ru>
2020-04-04 11:13:50 +0300

    Reply keyboard added to bot, доработал функцию Sym1. Bot v1.0.1

--- bot0.py
+++ bot0.py
@@ -251,7 +251,7 @@ def Sym(message):
 def Sym1(message):
     user_id = str(message.from_user.id)
     state = data['states'][user_id]
-    dosymmi = konvertiruem
+    dosymmi = koeficienti[12]
     if state == SYM1:
         symma = int(sym[user_id])
         cifra = message.text
